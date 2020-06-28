af2df5761da772526e6a646ad2da94d7025ce3a2
Hossein Yousefi <hossein@identi.app>
2020-06-28 20:42:38 +0200

    sound added

--- lib/home.dart
+++ lib/home.dart
@@ -1,42 +1,74 @@
 import 'dart:async';
 
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 
-class HomePage extends HookWidget {
-  static const wpm = 15;
+AudioCache audioCache = AudioCache(prefix: 'audios/');
+
+class HomePage extends StatefulWidget {
+  static const wpm = 10;
   static const dot = 1200 ~/ wpm;
 
   @override
+  _HomePageState createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  String message = '';
+  DateTime lastTap = DateTime.now();
+  AudioPlayer player;
+
+  @override
+  void initState() {
+    super.initState();
+    audioCache.load('beep.mp3');
+    audioCache.loop('beep.mp3').then((value) {
+      print('done!');
+      player = value;
+      player.pause();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final messageState = useState('');
-    final lastTapState = useState(DateTime.now());
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(messageState.value),
+            Expanded(child: Text(message)),
             GestureDetector(
               onTapDown: (details) {
-                lastTapState.value = DateTime.now();
+                player.resume();
+                print('hi');
+                lastTap = DateTime.now();
+                setState(() {});
               },
               onTapUp: (details) {
-                final diff = DateTime.now().difference(lastTapState.value);
-                if (diff.inMilliseconds <= dot) {
-                  messageState.value += '.';
+                player.pause();
+                print('bye');
+                final diff = DateTime.now().difference(lastTap);
+                if (diff.inMilliseconds <= HomePage.dot) {
+                  message += '.';
                 } else {
-                  messageState.value += '-';
+                  message += '-';
                 }
-                lastTapState.value = DateTime.now();
+                lastTap = DateTime.now();
                 Timer(
-                  const Duration(milliseconds: 7 * dot),
+                  const Duration(milliseconds: 7 * HomePage.dot),
                   () => {
-                    if (DateTime.now().difference(lastTapState.value).inMilliseconds >= 7 * dot)
-                      {messageState.value += ' '}
+                    if (DateTime.now().difference(lastTap).inMilliseconds >= 7 * HomePage.dot)
+                      {setState(() => message += ' ')}
                   },
                 );
+                setState(() {});
               },
+              onTapCancel: () => audioCache.fixedPlayer.pause(),
               child: CircleAvatar(
+                radius: 100,
                 backgroundColor: Colors.grey[700],
               ),
             ),
